b3ad0fcde4519465fea4bef498595fd8b207f7bf
KaiKimera <git@kai.kim>
2024-06-12 18:22:33 +0300

    2024-06-12 15:22:33

---
+++ docs/conf/ssh.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S bash -e
+
+cat="$( command -v cat )"
+
+${cat} > '/etc/ssh/sshd_config.d/sshd.local.conf' <<EOF
+Port 8022
+IgnoreRhosts yes
+MaxAuthTries 2
+PermitEmptyPasswords no
+PermitRootLogin no
+X11Forwarding no
+EOF
