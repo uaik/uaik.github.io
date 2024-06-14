4a9253415d705fbff9ac12ee47aaa746bab7c159
KaiKimera <git@kai.kim>
2024-06-15 02:31:53 +0300

    2024-06-14 23:31:53

--- docs/conf/kernel.zabbly.sh
+++ docs/conf/kernel.zabbly.sh
@@ -22,16 +22,16 @@ init() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 debian() {
-  local gpg_d='/etc/apt/keyrings'; local gpg_f='kernel.zabbly.gpg'; [[ ! -d "${gpg_d}" ]] && exit 1
+  local gpg_d='/etc/apt/keyrings'; local gpg_f='kernel.zabbly.asc'; [[ ! -d "${gpg_d}" ]] && exit 1
   local list_d='/etc/apt/sources.list.d'; local list_f='kernel.zabbly.sources'; [[ ! -d "${list_d}" ]] && exit 1
 
   ${curl} -fsSLo "${gpg_d}/${gpg_f}" 'https://pkgs.zabbly.com/key.asc'
   ${cat} > "${list_d}/${list_f}" <<EOF
 Enabled:        yes
-Types:          deb
+Types:          deb deb-src
 URIs:           https://pkgs.zabbly.com/kernel/stable
 Suites:         ${osCodeName}
-Components:     main
+Components:     main zfs
 Architectures:  $( dpkg --print-architecture )
 Signed-By:      ${gpg_d}/${gpg_f}
 EOF
