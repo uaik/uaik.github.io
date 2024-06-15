8f4cb144088559164f69702ab913d9672f4f6908
KaiKimera <git@kai.kim>
2024-06-15 14:35:09 +0300

    2024-06-15 11:35:09

--- docs/conf/kernel.zabbly.sh
+++ docs/conf/kernel.zabbly.sh
@@ -8,6 +8,7 @@ init() {
   # Apps.
   cat="$( command -v cat )"
   curl="$( command -v curl )"
+  gpg="$( command -v gpg )"
 
   # OS.
   osId=$(. '/etc/os-release' && echo "${ID}")
@@ -22,10 +23,10 @@ init() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 debian() {
-  local gpg_d='/etc/apt/keyrings'; local gpg_f='kernel.zabbly.asc'; [[ ! -d "${gpg_d}" ]] && exit 1
+  local gpg_d='/etc/apt/keyrings'; local gpg_f='kernel.zabbly.gpg'; [[ ! -d "${gpg_d}" ]] && exit 1
   local list_d='/etc/apt/sources.list.d'; local list_f='kernel.zabbly.sources'; [[ ! -d "${list_d}" ]] && exit 1
 
-  ${curl} -fsSLo "${gpg_d}/${gpg_f}" 'https://pkgs.zabbly.com/key.asc'
+  ${curl} -fsSL 'https://pkgs.zabbly.com/key.asc' | ${gpg} --dearmor > "${gpg_d}/${gpg_f}"
   ${cat} > "${list_d}/${list_f}" <<EOF
 X-Repolib-Name: Kernel (Zabbly)
 Enabled:        yes
