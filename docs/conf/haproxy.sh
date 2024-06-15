e9d7bfe645dabccbcd51d9e68a05068d87ee41dd
KaiKimera <git@kai.kim>
2024-06-15 14:46:15 +0300

    2024-06-15 11:46:15

--- docs/conf/haproxy.sh
+++ docs/conf/haproxy.sh
@@ -26,7 +26,7 @@ debian() {
   local gpg_d='/etc/apt/keyrings'; local gpg_f='haproxy.gpg'; [[ ! -d "${gpg_d}" ]] && exit 1
   local list_d='/etc/apt/sources.list.d'; local list_f='haproxy.sources'; [[ ! -d "${list_d}" ]] && exit 1
 
-  ${curl} -fsSL 'https://haproxy.debian.net/bernat.debian.org.gpg' | ${gpg} --dearmor > "${gpg_d}/${gpg_f}"
+  ${curl} -fsSL 'https://haproxy.debian.net/bernat.debian.org.gpg' | ${gpg} --dearmor -o "${gpg_d}/${gpg_f}"
   ${cat} > "${list_d}/${list_f}" <<EOF
 Enabled:        yes
 Types:          deb
--- docs/conf/kernel.zabbly.sh
+++ docs/conf/kernel.zabbly.sh
@@ -26,7 +26,7 @@ debian() {
   local gpg_d='/etc/apt/keyrings'; local gpg_f='kernel.zabbly.gpg'; [[ ! -d "${gpg_d}" ]] && exit 1
   local list_d='/etc/apt/sources.list.d'; local list_f='kernel.zabbly.sources'; [[ ! -d "${list_d}" ]] && exit 1
 
-  ${curl} -fsSL 'https://pkgs.zabbly.com/key.asc' | ${gpg} --dearmor > "${gpg_d}/${gpg_f}"
+  ${curl} -fsSL 'https://pkgs.zabbly.com/key.asc' | ${gpg} --dearmor -o "${gpg_d}/${gpg_f}"
   ${cat} > "${list_d}/${list_f}" <<EOF
 X-Repolib-Name: Kernel (Zabbly)
 Enabled:        yes
