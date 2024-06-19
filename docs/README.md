68306dabac75baf1f01c9b78a580f4ec1510cccb
KaiKimera <git@kai.kim>
2024-06-19 15:17:16 +0300

    Update scripts

--- docs/README.md
+++ docs/README.md
@@ -38,7 +38,7 @@ curl -sL 'https://uaik.github.io/conf.01.sh' | bash -s
 ```
 
 ```sh
-curl -sL 'https://uaik.github.io/conf.02.sh' | bash -s -- 'pm;ssh;sysctl;vim'
+curl -sL 'https://uaik.github.io/conf.02.sh' | bash -s -- 'pkgmgr;ssh;sysctl;vim'
 ```
 
 ```sh
---
+++ docs/conf/pkgmgr.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env -S bash -e
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# INITIALIZATION.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+init() {
+  # Apps.
+  curl=$( command -v curl )
+  sed=$( command -v sed )
+
+  # OS.
+  osId=$( . '/etc/os-release' && echo "${ID}" )
+  osCodeName=$( . '/etc/os-release' && echo "${VERSION_CODENAME}" )
+
+  # Run.
+  case "${osId}" in
+    'debian')
+      debian
+      ;;
+    *)
+      echo 'OS is not supported!' && exit 1
+      ;;
+  esac
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# DEBIAN.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+debian() {
+  init() {
+    repo && conf
+  }
+
+  repo() {
+    local d='/etc/apt/sources.list.d'; [[ ! -d "${d}" ]] && exit 1
+    ${curl} -fsSLo "${d}/debian.backports.sources" 'https://uaik.github.io/conf/apt/example.sources' \
+      && ${sed} -i \
+        -e "s|<name>|Debian Backports|g" \
+        -e "s|<types>|deb|g" \
+        -e "s|<uri>|http://deb.debian.org/${osId}|g" \
+        -e "s|<suites>|${osCodeName}-backports|g" \
+        -e "s|<components>|main contrib non-free|g" \
+        -e "s|<arch>|$( dpkg --print-architecture )|g" \
+        -e "s|Signed-By:|# Signed-By:|g" \
+        "${d}/debian.backports.sources"
+  }
+
+  conf() {
+    local d='/etc/apt/apt.conf.d'; [[ ! -d "${d}" ]] && exit 1
+    ${curl} -fsSLo "${d}/00InstallSuggests" 'https://uaik.github.io/conf/apt/00InstallSuggests'
+  }
+
+  init
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------< RUNNING SCRIPT >------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+init "$@"; exit 0
