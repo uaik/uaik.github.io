3f70338791c87856e3fe61f219b2850d469ffd7c
KaiKimera <git@kai.kim>
2024-06-20 20:41:13 +0000

    2024-06-20 20:41:13

--- docs/conf/nft.sh
+++ docs/conf/nft.sh
@@ -7,7 +7,6 @@ cmd_check() { command -v "${1}" > /dev/null 2>&1 || { echo >&2 "Required: '${1}'
 chmod=$( command -v 'chmod' ); cmd_check 'chmod'
 curl=$( command -v 'curl' ); cmd_check 'curl'
 mv=$( command -v 'mv' ); cmd_check 'mv'
-systemctl=$( command -v 'systemctl' ); cmd_check 'systemctl'
 
 # OS.
 osId=$( . '/etc/os-release' && echo "${ID}" )
@@ -33,9 +32,6 @@ debian() {
   conf() {
     local f='/etc/nftables.conf'; [[ -f "${f}" && ! -f "${f}.orig" ]] && ${mv} "${f}" "${f}.orig" || exit 1
     ${curl} -fsSLo "${f}" 'https://uaik.github.io/conf/nft/nftables.conf' && ${chmod} +x "${f}"
-
-    local s=( 'nftables' )
-    for i in "${s[@]}"; do ${systemctl} enable --now "${i}"; done
   }
 
   run
