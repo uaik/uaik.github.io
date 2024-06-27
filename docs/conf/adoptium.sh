650b6f406455e5a709c50ef4ce5930db7c1565ab
KaiKimera <git@kai.kim>
2024-06-27 22:27:03 +0000

    2024-06-27 22:27:03

--- docs/conf/adoptium.sh
+++ docs/conf/adoptium.sh
@@ -8,6 +8,7 @@ sed=$( command -v 'sed' )
 
 # OS.
 osId=$( . '/etc/os-release' && echo "${ID}" )
+osCodeName=$( . '/etc/os-release' && echo "${VERSION_CODENAME}" )
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # INITIALIZATION.
@@ -25,23 +26,23 @@ run() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 debian() {
-  run() { repo '20.x' && apt; }
+  run() { repo && apt '21-jdk'; }
 
   repo() {
     local gpg_d; gpg_d='/etc/apt/keyrings'; [[ ! -d "${gpg_d}" ]] && exit 1
-    local gpg_f; gpg_f='nodejs.gpg'
+    local gpg_f; gpg_f='adoptium.gpg'
     local list_d; list_d='/etc/apt/sources.list.d'; [[ ! -d "${list_d}" ]] && exit 1
-    local list_f; list_f='nodejs.sources'
-    local key; key='https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key'
+    local list_f; list_f='adoptium.sources'
+    local key; key='https://packages.adoptium.net/artifactory/api/gpg/key/public'
 
     ${curl} -fsSL "${key}" | ${gpg} --dearmor -o "${gpg_d}/${gpg_f}" \
       && ${curl} -fsSLo "${list_d}/${list_f}" 'https://uaik.github.io/conf/apt/deb.sources.tpl' \
       && ${sed} -i \
-        -e "s|<#_name_#>|Node.js|g" \
+        -e "s|<#_name_#>|Eclipse Temurin|g" \
         -e "s|<#_enabled_#>|yes|g" \
         -e "s|<#_types_#>|deb|g" \
-        -e "s|<#_uri_#>|https://deb.nodesource.com/node_${1}|g" \
-        -e "s|<#_suites_#>|nodistro|g" \
+        -e "s|<#_uri_#>|https://packages.adoptium.net/artifactory/deb|g" \
+        -e "s|<#_suites_#>|${osCodeName}|g" \
         -e "s|<#_components_#>|main|g" \
         -e "s|<#_arch_#>|$( dpkg --print-architecture )|g" \
         -e "s|<#_sig_#>|${gpg_d}/${gpg_f}|g" \
@@ -49,11 +50,7 @@ debian() {
   }
 
   apt() {
-    local apt_d; apt_d='/etc/apt/preferences.d'; [[ ! -d "${apt_d}" ]] && exit 1
-    local apt_f; apt_f=( 'nodejs.pref' 'nsolid.pref' )
-    for i in "${apt_f[@]}"; do ${curl} -fsSLo "${apt_d}/${i}" "https://uaik.github.io/conf/nodejs/${i}"; done
-
-    local p; p='nodejs'
+    local p; p="temurin-${1}"
     ${apt} update && ${apt} install --yes ${p}
   }
 
