64679fd5c08a71a834fad12979a83942bde71967
KaiKimera <git@kai.kim>
2024-06-27 22:17:14 +0000

    2024-06-27 22:17:14

---
+++ docs/conf/nodejs.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env -S bash -e
+
+# Apps.
+apt=$( command -v 'apt' )
+curl=$( command -v 'curl' )
+gpg=$( command -v 'gpg' )
+sed=$( command -v 'sed' )
+
+# OS.
+osId=$( . '/etc/os-release' && echo "${ID}" )
+osCodeName=$( . '/etc/os-release' && echo "${VERSION_CODENAME}" )
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# INITIALIZATION.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+run() {
+  case "${osId}" in
+    'debian') debian ;;
+    *) echo 'OS is not supported!' && exit 1 ;;
+  esac
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# DEBIAN.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+debian() {
+  run() { repo '20.x' && apt; }
+
+  repo() {
+    local gpg_d; gpg_d='/etc/apt/keyrings'; [[ ! -d "${gpg_d}" ]] && exit 1
+    local gpg_f; gpg_f='nodejs.gpg'
+    local list_d; list_d='/etc/apt/sources.list.d'; [[ ! -d "${list_d}" ]] && exit 1
+    local list_f; list_f='nodejs.sources'
+    local key; key='https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key'
+
+    ${curl} -fsSL "${key}" | ${gpg} --dearmor -o "${gpg_d}/${gpg_f}" \
+      && ${curl} -fsSLo "${list_d}/${list_f}" 'https://uaik.github.io/conf/apt/deb.sources.tpl' \
+      && ${sed} -i \
+        -e "s|<#_name_#>|Node.js|g" \
+        -e "s|<#_enabled_#>|yes|g" \
+        -e "s|<#_types_#>|deb|g" \
+        -e "s|<#_uri_#>|https://deb.nodesource.com/node_${1}|g" \
+        -e "s|<#_suites_#>|nodistro|g" \
+        -e "s|<#_components_#>|main|g" \
+        -e "s|<#_arch_#>|$( dpkg --print-architecture )|g" \
+        -e "s|<#_sig_#>|${gpg_d}/${gpg_f}|g" \
+        "${list_d}/${list_f}"
+  }
+
+  apt() {
+    local apt_d; apt_d='/etc/apt/preferences.d'; [[ ! -d "${apt_d}" ]] && exit 1
+    local apt_f; apt_f=( 'nodejs.pref' 'nsolid.pref' )
+    for i in "${apt_f[@]}"; do ${curl} -fsSLo "${apt_d}/${i}" "https://uaik.github.io/conf/nodejs/${i}"; done
+
+    local p; p='nodejs'
+    ${apt} update && ${apt} install --yes ${p}
+  }
+
+  run
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------< RUNNING SCRIPT >------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+run "$@"; exit 0
