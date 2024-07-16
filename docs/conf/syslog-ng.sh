2d15c9f62da113556eb8ffbfe1b5d498771aadbc
KaiKimera <git@kai.kim>
2024-07-16 12:53:41 +0300

    Upload syslog-ng

---
+++ docs/conf/syslog-ng.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env -S bash -e
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# OS.
+osId=$( . '/etc/os-release' && echo "${ID}" )
+osCodeName=$( . '/etc/os-release' && echo "${VERSION_CODENAME}" )
+
+# Apps.
+apt=$( command -v 'apt' )
+curl=$( command -v 'curl' )
+gpg=$( command -v 'gpg' )
+sed=$( command -v 'sed' )
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# INITIALIZATION.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+run() {
+  case "${osId}" in
+    'debian') debian ;;
+    *) echo 'OS is not supported!' && exit 1 ;;
+  esac
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# DEBIAN.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+debian() {
+  run() { repo && apt; }
+
+  repo() {
+    local gpg_d; gpg_d='/etc/apt/keyrings'; [[ ! -d "${gpg_d}" ]] && exit 1
+    local gpg_f; gpg_f='syslog-ng.gpg'
+    local list_d; list_d='/etc/apt/sources.list.d'; [[ ! -d "${list_d}" ]] && exit 1
+    local list_f; list_f='syslog-ng.sources'
+    local key; key='https://ose-repo.syslog-ng.com/apt/syslog-ng-ose-pub.asc'
+
+    ${curl} -fsSL "${key}" | ${gpg} --dearmor -o "${gpg_d}/${gpg_f}" \
+      && ${curl} -fsSLo "${list_d}/${list_f}" 'https://uaik.github.io/conf/apt/deb.sources.tpl' \
+      && ${sed} -i \
+        -e "s|<#_name_#>|Syslog-NG|g" \
+        -e "s|<#_enabled_#>|yes|g" \
+        -e "s|<#_types_#>|deb|g" \
+        -e "s|<#_uri_#>|https://ose-repo.syslog-ng.com/apt|g" \
+        -e "s|<#_suites_#>|stable|g" \
+        -e "s|<#_components_#>|debian-${osCodeName}|g" \
+        -e "s|<#_arch_#>|$( dpkg --print-architecture )|g" \
+        -e "s|<#_sig_#>|${gpg_d}/${gpg_f}|g" \
+        "${list_d}/${list_f}"
+  }
+
+  apt() {
+    local p; p=('syslog-ng')
+    ${apt} update && ${apt} install --yes "${p[@]}"
+  }
+
+  run
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------< RUNNING SCRIPT >------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+run && exit 0 || exit 1
