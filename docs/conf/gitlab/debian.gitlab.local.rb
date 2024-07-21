20c9260cb37acae075eb528b59ea04bec7425472
KaiKimera <git@kai.kim>
2024-07-21 14:01:53 +0000

    2024-07-21 14:01:53

--- docs/conf/gitlab/debian.gitlab.local.rb
+++ docs/conf/gitlab/debian.gitlab.local.rb
@@ -1,13 +1,13 @@
-external_url 'https://127.0.0.1'
+external_url 'http://127.0.0.1'
 user['username'] = 'gitlab'
 user['group'] = 'gitlab'
 letsencrypt['enable'] = false
 nginx['client_max_body_size'] = '250m'
-nginx['redirect_http_to_https'] = true
-nginx['ssl_certificate_key'] = '/etc/ssl/_ssc/auth.server.key'
-nginx['ssl_certificate'] = '/etc/ssl/_ssc/auth.server.crt'
-registry_nginx['redirect_http_to_https'] = true
-mattermost_nginx['redirect_http_to_https'] = true
+#nginx['redirect_http_to_https'] = true
+#nginx['ssl_certificate_key'] = '/etc/ssl/_ssc/auth.server.key'
+#nginx['ssl_certificate'] = '/etc/ssl/_ssc/auth.server.crt'
+#registry_nginx['redirect_http_to_https'] = true
+#mattermost_nginx['redirect_http_to_https'] = true
 gitlab_rails['gitlab_default_projects_features_builds'] = false
 gitlab_rails['omniauth_enabled'] = false
 gitlab_rails['smtp_enable'] = false
