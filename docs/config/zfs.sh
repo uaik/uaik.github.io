b48d818ad213424ecbb5a050d61ffa28c2c4aa61
KaiKimera <git@kai.kim>
2024-08-22 19:06:54 +0000

    2024-08-22 19:06:54

--- docs/config/zfs.sh
+++ docs/config/zfs.sh
@@ -44,19 +44,8 @@ debian() {
     local p00; p00=('linux-headers-amd64')
     local p01; p01=('zfsutils-linux' 'zfs-dkms' 'zfs-zed')
 
-<<<<<<< HEAD
-    apt install --yes "${p01[@]}" \
-      && apt install --yes -t 'stable-backports' "${p02[@]}"
-  }
-
-  man() {
-    echo '' && echo '--- [ZFS] Creating the Pool'
-    curl -fsSL 'https://uaik.github.io/config/zfs/man.txt'
-    echo ''
-=======
-    ${apt} install --yes "${p00[@]}" \
-      && ${apt} install --yes -t 'stable-backports' "${p01[@]}"
->>>>>>> a57709a6d782bb0644498bedd37e5f7909c6e64c
+    apt install --yes "${p00[@]}" \
+      && apt install --yes -t 'stable-backports' "${p01[@]}"
   }
 
   run
