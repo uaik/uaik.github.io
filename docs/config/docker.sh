182d2ff2f874998a8d8b96dc4a38b2c0be48e1b1
KaiKimera <git@kai.kim>
2024-08-25 14:00:15 +0000

    2024-08-25 14:00:15

--- docs/config/docker.sh
+++ docs/config/docker.sh
@@ -3,6 +3,7 @@
 
 # OS.
 osId=$( . '/etc/os-release' && echo "${ID}" )
+osCodeName=$( . '/etc/os-release' && echo "${VERSION_CODENAME}" )
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # INITIALIZATION
@@ -23,26 +24,26 @@ debian() {
   run() { repo && install; }
 
   repo() {
-    local sig; sig='/etc/apt/keyrings/caddy.gpg'; [[ ! -d "${sig%/*}" ]] && exit 1
-    local src; src='/etc/apt/sources.list.d/caddy.sources'; [[ ! -d "${src%/*}" ]] && exit 1
-    local key; key='https://dl.cloudsmith.io/public/caddy/stable/gpg.key'
+    local sig; sig='/etc/apt/keyrings/docker.gpg'; [[ ! -d "${sig%/*}" ]] && exit 1
+    local src; src='/etc/apt/sources.list.d/docker.sources'; [[ ! -d "${src%/*}" ]] && exit 1
+    local key; key='https://download.docker.com/linux/debian/gpg'
 
-    curl -fsSL "${key}" | gpg --dearmor -o "${sig}" \
+    curl -fsSLo "${sig}" "${key}" \
       && curl -fsSLo "${src}" 'https://uaik.github.io/config/apt/deb.sources.tpl' \
       && sed -i \
-        -e "s|<#_name_#>|Caddy|g" \
+        -e "s|<#_name_#>|Docker|g" \
         -e "s|<#_enabled_#>|yes|g" \
         -e "s|<#_types_#>|deb|g" \
-        -e "s|<#_uri_#>|https://dl.cloudsmith.io/public/caddy/stable/deb/debian|g" \
-        -e "s|<#_suites_#>|any-version|g" \
-        -e "s|<#_components_#>|main|g" \
+        -e "s|<#_uri_#>|https://download.docker.com/linux/debian|g" \
+        -e "s|<#_suites_#>|${osCodeName}|g" \
+        -e "s|<#_components_#>|stable|g" \
         -e "s|<#_arch_#>|$( dpkg --print-architecture )|g" \
         -e "s|<#_sig_#>|${sig}|g" \
         "${src}"
   }
 
   install() {
-    local p; p=('caddy')
+    local p; p=('docker-ce' 'docker-ce-cli' 'containerd.io' 'docker-buildx-plugin' 'docker-compose-plugin')
 
     apt update \
       && apt install --yes "${p[@]}"
