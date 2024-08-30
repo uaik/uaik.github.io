0f32e71d83592a00125c958aaefc0ae0681623db
KaiKimera <git@kai.kim>
2024-08-30 15:53:44 +0300

    Update scripts

--- docs/config/zabbix.sh
+++ docs/config/zabbix.sh
@@ -28,7 +28,7 @@ debian() {
     local src; src='/etc/apt/sources.list.d/zabbix.sources'; [[ ! -d "${src%/*}" ]] && exit 1
     local key; key='https://uaik.github.io/config/zabbix/zabbix.gpg'
 
-    curl -fsSL "${key}" | gpg --dearmor -o "${sig}" \
+    curl -fsSLo "${sig}" "${key}" \
       && curl -fsSLo "${src}" 'https://uaik.github.io/config/apt/deb.sources.tpl' \
       && sed -i \
         -e "s|<#_name_#>|Zabbix|g" \
