81cd62fd60bf32e94aa4bdab22a702be827275fe
KaiKimera <git@kai.kim>
2024-08-31 20:30:07 +0000

    2024-08-31 20:30:07

---
+++ docs/os/windows/auto.tpm.bat
@@ -0,0 +1,10 @@
+rem # BYPASSING SYSTEM REQUIREMENTS CHECKS (WINDOWS 11)
+rem # ---------------------------------------------------------------------------------------------------------------- #
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "BypassNRO" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassCPUCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassStorageCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /f /t "REG_DWORD" /d "1"
