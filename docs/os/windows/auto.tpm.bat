5c8374f50000b79a70c76a1c4f4fdcb41b03f922
KaiKimera <git@kai.kim>
2024-08-31 20:48:30 +0000

    2024-08-31 20:48:30

--- docs/os/windows/auto.tpm.bat
+++ docs/os/windows/auto.tpm.bat
@@ -1,10 +1,7 @@
 rem # BYPASSING SYSTEM REQUIREMENTS CHECKS (WINDOWS 11)
 rem # ---------------------------------------------------------------------------------------------------------------- #
 
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "BypassNRO" /f /t "REG_DWORD" /d "1"
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassCPUCheck" /f /t "REG_DWORD" /d "1"
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /f /t "REG_DWORD" /d "1"
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /f /t "REG_DWORD" /d "1"
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassStorageCheck" /f /t "REG_DWORD" /d "1"
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /f /t "REG_DWORD" /d "1"
-reg add "HKLM\SYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /t "REG_DWORD" /d "1" /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /t "REG_DWORD" /d "1" /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /t "REG_DWORD" /d "1" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "BypassNRO" /t "REG_DWORD" /d "1" /f
