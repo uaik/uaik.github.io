f9791e2bb2487ce8e17aff6d7f7210d1969fd2a6
KaiKimera <git@kai.kim>
2024-09-03 11:56:51 +0300

    Update scripts

---
+++ docs/config/timescaledb.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env -S bash -e
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# OS.
+osId=$( . '/etc/os-release' && echo "${ID}" )
+osCodeName=$( . '/etc/os-release' && echo "${VERSION_CODENAME}" )
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# INITIALIZATION
+# -------------------------------------------------------------------------------------------------------------------- #
+
+run() {
+  case "${osId}" in
+    'debian') debian ;;
+    *) echo 'OS is not supported!' && exit 1 ;;
+  esac
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# DEBIAN
+# -------------------------------------------------------------------------------------------------------------------- #
+
+debian() {
+  run() { repo && install '16'; }
+
+  repo() {
+    local sig; sig='/etc/apt/keyrings/timescaledb.gpg'; [[ ! -d "${sig%/*}" ]] && exit 1
+    local src; src='/etc/apt/sources.list.d/timescaledb.sources'; [[ ! -d "${src%/*}" ]] && exit 1
+    local key; key='https://packagecloud.io/timescale/timescaledb/gpgkey'
+
+    curl -fsSL "${key}" | gpg --dearmor -o "${sig}" \
+      && curl -fsSLo "${src}" 'https://uaik.github.io/config/apt/deb.sources.tpl' \
+      && sed -i \
+        -e "s|<#_name_#>|TimescaleDB|g" \
+        -e "s|<#_enabled_#>|yes|g" \
+        -e "s|<#_types_#>|deb|g" \
+        -e "s|<#_uri_#>|https://packagecloud.io/timescale/timescaledb/debian|g" \
+        -e "s|<#_suites_#>|${osCodeName}|g" \
+        -e "s|<#_components_#>|main|g" \
+        -e "s|<#_arch_#>|$( dpkg --print-architecture )|g" \
+        -e "s|<#_sig_#>|${sig}|g" \
+        "${src}"
+  }
+
+  install() {
+    local p; p=("timescaledb-2-postgresql-${1}")
+
+    apt update \
+      && apt install --yes "${p[@]}"
+  }
+
+  run
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------< RUNNING SCRIPT >------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+run && exit 0 || exit 1
