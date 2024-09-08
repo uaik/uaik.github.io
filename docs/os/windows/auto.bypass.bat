2523c08ba0e0a0c600cdbb08408f800d2a1c801a
KaiKimera <git@kai.kim>
2024-09-08 08:15:49 +0000

    2024-09-08 08:15:49

--- docs/os/windows/auto.bypass.bat
+++ docs/os/windows/auto.bypass.bat
@@ -1,4 +1,4 @@
-rem # BYPASSING SYSTEM REQUIREMENTS CHECKS (WINDOWS 11)
+rem # BYPASSING SYSTEM REQUIREMENTS CHECKS
 rem #
 rem # @package   CMD
 rem # @author    Kai Kimera <mail@kai.kim>
@@ -14,3 +14,5 @@ reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /t "REG_DWORD" /d 1 /f
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /t "REG_DWORD" /d 1 /f
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /t "REG_DWORD" /d 1 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "BypassNRO" /t "REG_DWORD" /d 1 /f
+
+exit
