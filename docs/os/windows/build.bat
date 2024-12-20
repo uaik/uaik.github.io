bddbe503e5fa0fbaec26f286a3125a0e0305aa38
KaiKimera <git@kai.kim>
2024-12-20 12:18:39 +0300

    Update scripts

--- docs/os/windows/build.bat
+++ docs/os/windows/build.bat
@@ -1,32 +1,46 @@
 @echo off
 
-set index=2 4
+set "idx=%*"
 set "drv=%~dp0drv"
 set "mnt=%~dp0mnt"
-set "wim=%~dp0wim"
 set "pkg=%~dp0pkg"
+set "wim=%~dp0wim"
 
 rem # ---------------------------------------------------------------------------------------------------------------- #
-rem # boot.wim
+rem # CREATE STRUCTURE
 rem # ---------------------------------------------------------------------------------------------------------------- #
 
-if exist "%wim%\boot.wim" (
-  Dism /Mount-Image /ImageFile:"%wim%\boot.wim" /Index:2 /MountDir:"%mnt%"
-) else (
-  exit /b 1
+if "%1" == "struct" (
+  if not exist "%drv%" md "%drv%"
+  if not exist "%mnt%" md "%mnt%"
+  if not exist "%pkg%" md "%pkg%"
+  if not exist "%wim%" md "%wim%"
+  exit
 )
 
-if exist "%drv%" (
-  Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
+rem # ---------------------------------------------------------------------------------------------------------------- #
+rem # BOOT.WIM
+rem # ---------------------------------------------------------------------------------------------------------------- #
+
+for %%i in (2) do (
+  if exist "%wim%\boot.wim" (
+    Dism /Mount-Image /ImageFile:"%wim%\boot.wim" /Index:%%i /MountDir:"%mnt%"
+  ) else (
+    exit /b 1
+  )
+
+  if exist "%drv%" (
+    Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
+  )
+
+  Dism /Unmount-Image /MountDir:"%mnt%" /Commit && timeout /t 5 /nobreak > nul
 )
 
-Dism /Unmount-Image /MountDir:"%mnt%" /Commit
-
 rem # ---------------------------------------------------------------------------------------------------------------- #
-rem # install.wim
+rem # INSTALL.WIM
 rem # ---------------------------------------------------------------------------------------------------------------- #
 
-for %%i in (%index%) do (
+for %%i in (%idx%) do (
   if exist "%wim%\install.wim" (
     Dism /Mount-Image /ImageFile:"%wim%\install.wim" /Index:%%i /MountDir:"%mnt%"
   ) else (
@@ -48,7 +62,7 @@ for %%i in (%index%) do (
     )
   )
 
-  Dism /Unmount-Image /MountDir:"%mnt%" /Commit
+  Dism /Unmount-Image /MountDir:"%mnt%" /Commit && timeout /t 5 /nobreak > nul
 )
 
 exit
