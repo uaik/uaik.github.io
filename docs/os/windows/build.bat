f3dc87b4175c71738c4657d896f739d4791c1d6a
KaiKimera <git@kai.kim>
2024-12-20 12:33:14 +0300

    Update scripts

--- docs/os/windows/build.bat
+++ docs/os/windows/build.bat
@@ -24,15 +24,18 @@ rem # --------------------------------------------------------------------------
 
 for %%i in (2) do (
   if exist "%wim%\boot.wim" (
+    echo Mounting a Windows image...
     Dism /Mount-Image /ImageFile:"%wim%\boot.wim" /Index:%%i /MountDir:"%mnt%"
   ) else (
     exit /b 1
   )
 
   if exist "%drv%" (
+    echo Integrating Windows Drivers...
     Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
   )
 
+  echo Saving Windows image...
   Dism /Unmount-Image /MountDir:"%mnt%" /Commit && timeout /t 5 /nobreak > nul
 )
 
@@ -42,26 +45,33 @@ rem # --------------------------------------------------------------------------
 
 for %%i in (%idx%) do (
   if exist "%wim%\install.wim" (
+    echo Mounting a Windows image...
     Dism /Mount-Image /ImageFile:"%wim%\install.wim" /Index:%%i /MountDir:"%mnt%"
   ) else (
     exit /b 1
   )
 
   if exist "%drv%" (
+    echo Integrating Windows Drivers...
     Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
   )
 
   if exist "%pkg%" (
     if exist "%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab" (
+      echo Integrating Windows Client Language Packs...
       Dism /Image:"%mnt%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab"
+      echo Integration of additional Windows language packs...
       Dism /Image:"%mnt%" /Add-Capability /CapabilityName:Language.Basic~~~ru-ru~0.0.1.0 /CapabilityName:Language.Handwriting~~~ru-ru~0.0.1.0 /CapabilityName:Language.OCR~~~ru-ru~0.0.1.0 /CapabilityName:Language.Speech~~~ru-ru~0.0.1.0 /CapabilityName:Language.TextToSpeech~~~ru-ru~0.0.1.0 /Source:"%pkg%"
     )
     if exist "%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab" (
+      echo Integrating Windows Server Language Packs...
       Dism /Image:"%mnt%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab"
+      echo Integration of additional Windows language packs...
       Dism /Image:"%mnt%" /Add-Capability /CapabilityName:Language.Basic~~~ru-ru~0.0.1.0 /CapabilityName:Language.Handwriting~~~ru-ru~0.0.1.0 /CapabilityName:Language.OCR~~~ru-ru~0.0.1.0 /CapabilityName:Language.Speech~~~ru-ru~0.0.1.0 /CapabilityName:Language.TextToSpeech~~~ru-ru~0.0.1.0 /Source:"%pkg%"
     )
   )
 
+  echo Saving Windows image...
   Dism /Unmount-Image /MountDir:"%mnt%" /Commit && timeout /t 5 /nobreak > nul
 )
 
