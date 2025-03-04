a8dd5d9d2398d94b26b0d2fe6d8323d786702ddd
KaiKimera <git@kai.kim>
2025-03-04 13:45:01 +0300

    Update scripts

--- docs/config/ssh.sh
+++ docs/config/ssh.sh
@@ -13,7 +13,7 @@ run() { config; }
 
 config() {
   local d; d='/etc/ssh/sshd_config.d'; [[ ! -d "${d}" ]] && exit 1
-  local f; f=('00-sshd.local.conf')
+  local f; f=('90-sshd.local.conf')
 
   for i in "${f[@]}"; do
     curl -fsSLo "${d}/${i}" "https://uaik.github.io/config/ssh/${i}"
