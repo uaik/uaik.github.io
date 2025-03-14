b08f654b9f8639f77a060326bbce7751b77ba5a6
KaiKimera <git@kai.kim>
2025-03-14 08:14:57 +0000

    2025-03-14 08:14:57

--- docs/os/windows/build.cmd
+++ docs/os/windows/build.cmd
@@ -1,10 +1,11 @@
 @echo off
 
-set "idx=%*"
+rem # set "idx=%*"
 set "cap=%~dp0cap"
 set "drv=%~dp0drv"
 set "mnt=%~dp0mnt"
 set "pkg=%~dp0pkg"
+set "tmp=%~dp0tmp"
 set "upd=%~dp0upd"
 set "wim=%~dp0wim"
 
@@ -13,7 +14,7 @@ rem # CREATE STRUCTURE
 rem # ---------------------------------------------------------------------------------------------------------------- #
 
 if "%1" == "struct" (
-  for %%i in ("%drv%" "%mnt%" "%pkg%" "%wim%") do (
+  for %%i in ("%drv%" "%mnt%" "%pkg%" "%tmp%" "%wim%") do (
     if not exist "%%i" md "%%i"
   )
 
@@ -25,65 +26,71 @@ rem # --------------------------------------------------------------------------
 rem # BOOT.WIM
 rem # ---------------------------------------------------------------------------------------------------------------- #
 
-for %%i in (2) do (
-  if exist "%wim%\boot.wim" (
+if exist "%wim%\boot.wim" (
+  for %%i in (2) do (
     echo: && echo Mounting a Windows image...
     Dism /Mount-Image /ImageFile:"%wim%\boot.wim" /Index:%%i /MountDir:"%mnt%"
 
     if exist "%drv%" (
       echo: && echo Integrating Windows Drivers...
-      Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
+      Dism /Image:"%mnt%" /ScratchDir:"%tmp%" /Add-Driver /Driver:"%drv%" /Recurse
     )
 
     echo: && echo Saving Windows image...
     Dism /Unmount-Image /MountDir:"%mnt%" /Commit
-  ) else (
-    echo boot.wim not found!
-    exit /b 1
   )
+) else (
+  echo: && echo BOOT.WIM not found!
+  exit /b 1
 )
 
 rem # ---------------------------------------------------------------------------------------------------------------- #
 rem # INSTALL.WIM
 rem # ---------------------------------------------------------------------------------------------------------------- #
 
-for %%i in (%idx%) do (
-  if exist "%wim%\install.wim" (
+if exist "%wim%\install.wim" (
+  Dism /Get-ImageInfo /ImageFile:"%wim%\install.wim"
+  set /p "idx=Enter ID: "
+
+  for %%i in (%idx%) do (
     echo: && echo Mounting a Windows image...
-    Dism /Mount-Image /ImageFile:"%wim%\install.wim" /Index:%%i /MountDir:"%mnt%"
+    Dism /Mount-Image /ImageFile:"%wim%\install.wim" /Index:%%i /MountDir:"%mnt%" && Dism /Get-MountedImageInfo
 
     if exist "%drv%" (
       echo: && echo Integrating Windows Drivers...
-      Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
+      Dism /Image:"%mnt%" /ScratchDir:"%tmp%" /Add-Driver /Driver:"%drv%" /Recurse
     )
 
     if exist "%pkg%" (
       if exist "%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab" (
         echo: && echo Integrating Windows Client Language Packs...
-        Dism /Image:"%mnt%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab"
+        Dism /Image:"%mnt%" /ScratchDir:"%tmp%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab"
       )
       if exist "%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab" (
         echo: && echo Integrating Windows Server Language Packs...
-        Dism /Image:"%mnt%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab"
+        Dism /Image:"%mnt%" /ScratchDir:"%tmp%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab"
       )
     )
 
     if exist "%cap%" (
       echo: && echo Integrating Windows capabilities...
-      Dism /Image:"%mnt%" /Add-Capability /CapabilityName:Language.Basic~~~ru-ru~0.0.1.0 /CapabilityName:Language.Handwriting~~~ru-ru~0.0.1.0 /CapabilityName:Language.OCR~~~ru-ru~0.0.1.0 /CapabilityName:Language.Speech~~~ru-ru~0.0.1.0 /CapabilityName:Language.TextToSpeech~~~ru-ru~0.0.1.0 /Source:"%cap%"
+      Dism /Image:"%mnt%" /ScratchDir:"%tmp%" /Add-Capability /CapabilityName:Language.Basic~~~ru-ru~0.0.1.0 /CapabilityName:Language.Handwriting~~~ru-ru~0.0.1.0 /CapabilityName:Language.OCR~~~ru-ru~0.0.1.0 /CapabilityName:Language.Speech~~~ru-ru~0.0.1.0 /CapabilityName:Language.TextToSpeech~~~ru-ru~0.0.1.0 /Source:"%cap%"
     )
 
     if exist "%upd%" (
       echo: && echo Integrating Windows Updates...
-      Dism /Image:"%mnt%" /Add-Package /PackagePath:"%upd%"
+      Dism /Image:"%mnt%" /ScratchDir:"%tmp%" /Add-Package /PackagePath:"%upd%"
     )
+
+    echo: && echo Reduce the Size of the Component Store...
+    Dism /Image:"%mnt%" /ScratchDir:"%tmp%" /Cleanup-Image /StartComponentCleanup /ResetBase
 
     echo: && echo Saving Windows image...
     Dism /Unmount-Image /MountDir:"%mnt%" /Commit
-  ) else (
-    echo install.wim not found!
-    exit /b 1
   )
+) else (
+  echo: && echo INSTALL.WIM not found!
+  exit /b 1
 )
 
 rem # ---------------------------------------------------------------------------------------------------------------- #
