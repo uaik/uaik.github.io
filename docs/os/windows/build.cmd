28607ad095d6b450d2ac98ffc9e4b087ab3d8a6d
KaiKimera <git@kai.kim>
2025-03-14 07:39:39 +0000

    2025-03-14 07:39:39

--- docs/os/windows/build.cmd
+++ docs/os/windows/build.cmd
@@ -16,6 +16,8 @@ if "%1" == "struct" (
   for %%i in ("%drv%" "%mnt%" "%pkg%" "%wim%") do (
     if not exist "%%i" md "%%i"
   )
+
+  echo The directory structure has been created!
   exit /b 0
 )
 
@@ -27,17 +29,18 @@ for %%i in (2) do (
   if exist "%wim%\boot.wim" (
     echo: && echo Mounting a Windows image...
     Dism /Mount-Image /ImageFile:"%wim%\boot.wim" /Index:%%i /MountDir:"%mnt%"
+
+    if exist "%drv%" (
+      echo: && echo Integrating Windows Drivers...
+      Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
+    )
+
+    echo: && echo Saving Windows image...
+    Dism /Unmount-Image /MountDir:"%mnt%" /Commit
   ) else (
+    echo boot.wim not found!
     exit /b 1
   )
-
-  if exist "%drv%" (
-    echo: && echo Integrating Windows Drivers...
-    Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
-  )
-
-  echo: && echo Saving Windows image...
-  Dism /Unmount-Image /MountDir:"%mnt%" /Commit
 )
 
 rem # ---------------------------------------------------------------------------------------------------------------- #
@@ -48,39 +51,39 @@ for %%i in (%idx%) do (
   if exist "%wim%\install.wim" (
     echo: && echo Mounting a Windows image...
     Dism /Mount-Image /ImageFile:"%wim%\install.wim" /Index:%%i /MountDir:"%mnt%"
+
+    if exist "%drv%" (
+      echo: && echo Integrating Windows Drivers...
+      Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
+    )
+
+    if exist "%pkg%" (
+      if exist "%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab" (
+        echo: && echo Integrating Windows Client Language Packs...
+        Dism /Image:"%mnt%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab"
+      )
+      if exist "%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab" (
+        echo: && echo Integrating Windows Server Language Packs...
+        Dism /Image:"%mnt%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab"
+      )
+    )
+
+    if exist "%cap%" (
+      echo: && echo Integrating Windows capabilities...
+      Dism /Image:"%mnt%" /Add-Capability /CapabilityName:Language.Basic~~~ru-ru~0.0.1.0 /CapabilityName:Language.Handwriting~~~ru-ru~0.0.1.0 /CapabilityName:Language.OCR~~~ru-ru~0.0.1.0 /CapabilityName:Language.Speech~~~ru-ru~0.0.1.0 /CapabilityName:Language.TextToSpeech~~~ru-ru~0.0.1.0 /Source:"%cap%"
+    )
+
+    if exist "%upd%" (
+      echo: && echo Integrating Windows Updates...
+      Dism /Image:"%mnt%" /Add-Package /PackagePath:"%upd%"
+    )
+
+    echo: && echo Saving Windows image...
+    Dism /Unmount-Image /MountDir:"%mnt%" /Commit
   ) else (
     echo install.wim not found!
     exit /b 1
   )
-
-  if exist "%drv%" (
-    echo: && echo Integrating Windows Drivers...
-    Dism /Image:"%mnt%" /Add-Driver /Driver:"%drv%" /Recurse
-  )
-
-  if exist "%pkg%" (
-    if exist "%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab" (
-      echo: && echo Integrating Windows Client Language Packs...
-      Dism /Image:"%mnt%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Client-Language-Pack_x64_ru-ru.cab"
-    )
-    if exist "%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab" (
-      echo: && echo Integrating Windows Server Language Packs...
-      Dism /Image:"%mnt%" /Add-Package /PackagePath:"%pkg%\Microsoft-Windows-Server-Language-Pack_x64_ru-ru.cab"
-    )
-  )
-
-  if exist "%cap%" (
-    echo: && echo Integrating Windows capabilities...
-    Dism /Image:"%mnt%" /Add-Capability /CapabilityName:Language.Basic~~~ru-ru~0.0.1.0 /CapabilityName:Language.Handwriting~~~ru-ru~0.0.1.0 /CapabilityName:Language.OCR~~~ru-ru~0.0.1.0 /CapabilityName:Language.Speech~~~ru-ru~0.0.1.0 /CapabilityName:Language.TextToSpeech~~~ru-ru~0.0.1.0 /Source:"%cap%"
-  )
-
-  if exist "%upd%" (
-    echo: && echo Integrating Windows Updates...
-    Dism /Image:"%mnt%" /Add-Package /PackagePath:"%upd%"
-  )
-
-  echo: && echo Saving Windows image...
-  Dism /Unmount-Image /MountDir:"%mnt%" /Commit
 )
 
 rem # ---------------------------------------------------------------------------------------------------------------- #
