2e0269e40436ffb2a8f29624170b1aca685e6f4f
KaiKimera <git@kai.kim>
2025-03-18 20:35:35 +0000

    2025-03-18 20:35:35

---
+++ docs/os/windows/config.cmd
@@ -0,0 +1,14 @@
+@echo off
+if "%1" == "11" (
+  reg add "HKLM\SYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /t "REG_DWORD" /d 1 /f
+  reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "BypassNRO" /t "REG_DWORD" /d 1 /f
+)
+sfc /scannow
+reg load "HKU\DefaultUser" "C:\Users\Default\NTUSER.DAT"
+reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t "REG_DWORD" /d 0 /f
+reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SeparateProcess" /t "REG_DWORD" /d 1 /f
+reg add "HKU\DefaultUser\Software\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /t "REG_DWORD" /d 1 /f
+reg unload "HKU\DefaultUser"
+netsh advfirewall firewall set rule group="@FirewallAPI.dll,-28752" new enable=Yes
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v "fDenyTSConnections" /t "REG_DWORD" /d 0 /f
+exit
