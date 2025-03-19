cbd7c1c38e1255ddd82a9476053b86877a629626
KaiKimera <git@kai.kim>
2025-03-19 11:51:36 +0000

    2025-03-19 11:51:35

--- docs/os/windows/config.cmd
+++ docs/os/windows/config.cmd
@@ -7,7 +7,6 @@ sfc /scannow
 reg load "HKU\DefaultUser" "C:\Users\Default\NTUSER.DAT"
 reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t "REG_DWORD" /d 0 /f
 reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SeparateProcess" /t "REG_DWORD" /d 1 /f
-reg add "HKU\DefaultUser\Software\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /t "REG_DWORD" /d 1 /f
 reg unload "HKU\DefaultUser"
 netsh advfirewall firewall set rule group="@FirewallAPI.dll,-28752" new enable=Yes
 reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v "fDenyTSConnections" /t "REG_DWORD" /d 0 /f
