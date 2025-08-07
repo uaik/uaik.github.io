8ef5b1f8027bd3d85aa3b0b0e4fb381c3a7034f1
KaiKimera <git@kai.kim>
2025-08-07 14:22:35 +0300

    Update resources

--- docs/os/windows/diskpart.cmd
+++ docs/os/windows/diskpart.cmd
@@ -23,11 +23,11 @@ if "%ErrorLevel%" EQU 0 (
     echo create partition primary size=512
     echo format quick fs=ntfs label="SYSTEM"
     echo active
+    echo create partition primary size=128
+    echo set id=17
     echo create partition primary size=1024
     echo format quick fs=ntfs label="RECOVERY"
     echo set id=27
-    echo create partition primary size=100
-    echo set id=17 override
   )
   echo create partition primary
   echo format quick fs=ntfs label="OS"
