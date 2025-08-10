30785de9f634685a672ee7cb3573e8532081f19b
KaiKimera <git@kai.kim>
2025-08-10 21:09:26 +0000

    2025-08-10 21:09:26

--- docs/os/windows/config.cmd
+++ docs/os/windows/config.cmd
@@ -13,17 +13,13 @@ if "%1" == "specialize" (
     reg add "HKLM\System\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /t "REG_DWORD" /d 1 /f
   )
   sfc /scannow
-  rem ---
   reg load "HKU\DefaultUser" "C:\Users\Default\NTUSER.DAT"
   reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t "REG_DWORD" /d 0 /f
   reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SeparateProcess" /t "REG_DWORD" /d 1 /f
   reg unload "HKU\DefaultUser"
-  rem ---
   netsh advfirewall firewall set rule group="@FirewallAPI.dll,-28752" new enable=Yes
   reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v "fDenyTSConnections" /t "REG_DWORD" /d 0 /f
-  rem ---
   rem reg add "HKLM\System\CurrentControlSet\Control\FileSystem" /v "LongPathsEnabled" /t "REG_DWORD" /d 1 /f
-  rem ---
   if exist "C:\Windows.old" rmdir /s /q "C:\Windows.old"
 )
 
