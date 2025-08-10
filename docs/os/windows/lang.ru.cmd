4af40f7716e84268a87511651aadaaba83260c25
KaiKimera <git@kai.kim>
2025-08-10 22:09:11 +0000

    2025-08-10 22:09:11

--- docs/os/windows/lang.ru.cmd
+++ docs/os/windows/lang.ru.cmd
@@ -1,5 +1,6 @@
 @echo off
-bcdedit /set {current} locale ru-RU
-bcdboot %WinDir% /l ru-RU
+
+bcdedit /set {current} locale ru-RU & bcdboot %WinDir% /l ru-RU
 reg delete "HKLM\SYSTEM\CurrentControlSet\Control\MUI\UILanguages\en-US" /f
+
 exit
