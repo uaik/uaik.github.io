af8813ea111299ce44ec450b40c3c3e2e158ae26
KaiKimera <git@kai.kim>
2025-09-18 14:07:18 +0300

    Update resources

--- docs/os/windows/virtio.cmd
+++ docs/os/windows/virtio.cmd
@@ -5,8 +5,8 @@ set "ver=%2"
 set "drv=fwcfg pvpanic vioinput viomem viorng vioscsi vioserial viostor"
 
 for %%i in (%drv%) do (
-  if not exist "%~dp0drv\%%i" md "%~dp0drv\%%i"
-  robocopy "%dsk%:\%%i\%ver%\amd64" "%~dp0drv\%%i" /e /is /it /im
+  if not exist "%~dp0drv\virtio\%%i" md "%~dp0drv\virtio\%%i"
+  robocopy "%dsk%:\%%i\%ver%\amd64" "%~dp0drv\virtio\%%i" /e /is /it /im
 )
 
 exit /b 0
