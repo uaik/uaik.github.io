4982b9e67f5d704a6a771daa619751e17db0a9e6
KaiKimera <git@kai.kim>
2025-09-19 17:01:03 +0300

    Update resources

--- docs/os/windows/build.cmd
+++ docs/os/windows/build.cmd
@@ -8,7 +8,7 @@ set "pkg=%~dp0pkg"
 set "tmp=%~dp0tmp"
 set "upd=%~dp0upd"
 set "wim=%~dp0wim"
-set "lang=af-za am-et ar-sa as-in az-latn-az ba-ru be-by bg-bg bn-bd bn-in br-fr bs-latn-ba ca-es cs-cz cy-gb da-dk de-ch de-de el-gr en-au en-ca en-gb en-in es-es es-mx es-us et-ee eu-es fa-ir fi-fi fil-ph fo-fo fr-be fr-ca fr-ch fr-fr ga-ie gd-gb gl-es gu-in ha-latn-ng haw-us he-il hi-in hr-hr hu-hu hy-am id-id ig-ng is-is it-it ja-jp ka-ge kk-kz kl-gl km-kh kn-in ko-kr kok-deva-in ky-kg lb-lu lo-la lt-lt lv-lv mi-nz mk-mk ml-in mn-mn mr-in ms-bn ms-my mt-mt my-mm nb-no ne-np nl-nl nn-no nso-za or-in pa-arab-pk pa-in pl-pl ps-af pt-br pt-pt quc-latn-gt quz-bo rm-ch ro-ro ru-ru rw-rw sah-ru sd-arab-pk si-lk sk-sk sl-si so-so sq-al sr-cyrl-rs sr-latn-rs sv-se sw-ke ta-in te-in tg-cyrl-tj th-th tk-tm tn-za tr-tr tt-ru ug-cn uk-ua ur-pk uz-latn-uz vi-vn wo-sn xh-za yo-ng zh-cn zh-hk zh-tw zu-za"
+set "lang=ru-ru zh-cn"
 set "font=Arab~~~und-ARAB~0.0.1.0 Beng~~~und-BENG~0.0.1.0 Cans~~~und-CANS~0.0.1.0 Cher~~~und-CHER~0.0.1.0 Deva~~~und-DEVA~0.0.1.0 Ethi~~~und-ETHI~0.0.1.0 Gujr~~~und-GUJR~0.0.1.0 Guru~~~und-GURU~0.0.1.0 Hans~~~und-HANS~0.0.1.0 Hant~~~und-HANT~0.0.1.0 Hebr~~~und-HEBR~0.0.1.0 Jpan~~~und-JPAN~0.0.1.0 Khmr~~~und-KHMR~0.0.1.0 Knda~~~und-KNDA~0.0.1.0 Kore~~~und-KORE~0.0.1.0 Laoo~~~und-LAOO~0.0.1.0 Mlym~~~und-MLYM~0.0.1.0 Orya~~~und-ORYA~0.0.1.0 PanEuropeanSupplementalFonts~~~~0.0.1.0 Sinh~~~und-SINH~0.0.1.0 Syrc~~~und-SYRC~0.0.1.0 Taml~~~und-TAML~0.0.1.0 Telu~~~und-TELU~0.0.1.0 Thai~~~und-THAI~0.0.1.0"
 
 rem # ---------------------------------------------------------------------------------------------------------------- #
