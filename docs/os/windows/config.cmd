bbc0aa7bee1b4e3f28c28a7b8e2b038e199d3e4e
KaiKimera <git@kai.kim>
2026-07-30 16:11:30 +0300

    Update resources

--- docs/os/windows/config.cmd
+++ docs/os/windows/config.cmd
@@ -3,16 +3,19 @@
 sfc /scannow
 rem ---
 reg load "HKU\DefaultUser" "C:\Users\Default\NTUSER.DAT"
-reg add "HKU\DefaultUser\Software\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /t "REG_DWORD" /d 1 /f
 reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t "REG_DWORD" /d 0 /f
 reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SeparateProcess" /t "REG_DWORD" /d 1 /f
+reg add "HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarAl" /t "REG_DWORD" /d 0 /f
+reg add "HKU\DefaultUser\Software\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /t "REG_DWORD" /d 1 /f
 reg unload "HKU\DefaultUser"
 rem ---
 netsh advfirewall firewall set rule group="@FirewallAPI.dll,-28752" new enable=Yes
 reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v "fDenyTSConnections" /t "REG_DWORD" /d 0 /f
 rem ---
-rem reg add "HKLM\System\CurrentControlSet\Control\FileSystem" /v "LongPathsEnabled" /t "REG_DWORD" /d 1 /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t "REG_DWORD" /d 1 /f
+reg add "HKLM\Software\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t "REG_DWORD" /d 0 /f
+reg add "HKLM\Software\Policies\Microsoft\Edge" /v "HideFirstRunExperience" /t "REG_DWORD" /d 1 /f
 rem ---
 if exist "C:\Windows.old" rmdir /s /q "C:\Windows.old"
-
+rem ---
 exit
