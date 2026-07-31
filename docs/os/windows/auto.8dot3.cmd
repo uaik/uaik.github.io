f268ad91ecfcc8f0ba2b605e39cd04a028e36968
KaiKimera <git@kai.kim>
2026-07-31 13:47:29 +0300

    Update resources

---
+++ docs/os/windows/auto.8dot3.cmd
@@ -0,0 +1,6 @@
+@echo off
+fsutil 8dot3name set X: 1 && fsutil 8dot3name strip /s /f X:\
+reg load "HKLM\mount" "X:\Windows\System32\config\SYSTEM"
+reg add "HKLM\mount\ControlSet001\Control\FileSystem" /v "NtfsDisable8dot3NameCreation" /t "REG_DWORD" /d "1" /f
+reg unload "HKLM\mount"
+exit
