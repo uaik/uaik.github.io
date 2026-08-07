bbcbc9a2086bfbad4b72c87f5424f5b55b9b89ed
KaiKimera <git@kai.kim>
2026-08-07 18:38:56 +0300

    Update resources

--- docs/os/windows/auto.specialize.cmd
+++ docs/os/windows/auto.specialize.cmd
@@ -11,36 +11,36 @@ reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v "fDenyTSConne
 :: Prevent download and installation of applications associated with certain hardware devices.
 :: ---------------------------------------------------------------------------------------------------------------------
 
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t "REG_DWORD" /d "1" /f
+:: reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t "REG_DWORD" /d "1" /f
 
 :: ---------------------------------------------------------------------------------------------------------------------
 :: Disable widgets.
 :: ---------------------------------------------------------------------------------------------------------------------
 
-reg add "HKLM\Software\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t "REG_DWORD" /d "0" /f
+:: reg add "HKLM\Software\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t "REG_DWORD" /d "0" /f
 
 :: ---------------------------------------------------------------------------------------------------------------------
 :: Hide Edge First Run Experience.
 :: ---------------------------------------------------------------------------------------------------------------------
 
-reg add "HKLM\Software\Policies\Microsoft\Edge" /v "HideFirstRunExperience" /t "REG_DWORD" /d "1" /f
+:: reg add "HKLM\Software\Policies\Microsoft\Edge" /v "HideFirstRunExperience" /t "REG_DWORD" /d "1" /f
 
 :: ---------------------------------------------------------------------------------------------------------------------
 :: Disable app suggestions.
 :: ---------------------------------------------------------------------------------------------------------------------
 
-reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /t "REG_DWORD" /d "1" /f
+:: reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /t "REG_DWORD" /d "1" /f
 
 :: ---------------------------------------------------------------------------------------------------------------------
 :: Disable SmartScreen in Windows and Edge.
 :: ---------------------------------------------------------------------------------------------------------------------
 
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "SmartScreenEnabled" /t "REG_SZ" /d "Off" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\WTDS\Components" /v "ServiceEnabled" /t "REG_DWORD" /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\WTDS\Components" /v "NotifyMalicious" /t "REG_DWORD" /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\WTDS\Components" /v "NotifyPasswordReuse" /t "REG_DWORD" /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\WTDS\Components" /v "NotifyUnsafeApp" /t "REG_DWORD" /d "0" /f
-reg add "HKLM\Software\Policies\Microsoft\Windows Defender Security Center\Systray" /v "HideSystray" /t "REG_DWORD" /d "1" /f
+:: reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "SmartScreenEnabled" /t "REG_SZ" /d "Off" /f
+:: reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\WTDS\Components" /v "ServiceEnabled" /t "REG_DWORD" /d "0" /f
+:: reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\WTDS\Components" /v "NotifyMalicious" /t "REG_DWORD" /d "0" /f
+:: reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\WTDS\Components" /v "NotifyPasswordReuse" /t "REG_DWORD" /d "0" /f
+:: reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\WTDS\Components" /v "NotifyUnsafeApp" /t "REG_DWORD" /d "0" /f
+:: reg add "HKLM\Software\Policies\Microsoft\Windows Defender Security Center\Systray" /v "HideSystray" /t "REG_DWORD" /d "1" /f
 
 :: ---------------------------------------------------------------------------------------------------------------------
 :: Tools.
