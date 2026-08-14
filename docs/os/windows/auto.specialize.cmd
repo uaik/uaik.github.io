2987a4eb44e9790cb07adf02d1031ea831ea84da
KaiKimera <git@kai.kim>
2026-08-14 13:38:42 +0300

    Update resources

--- docs/os/windows/auto.specialize.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-netsh advfirewall firewall set rule group="@FirewallAPI.dll,-28752" new enable=Yes
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v "fDenyTSConnections" /t "REG_DWORD" /d "0" /f
-sfc /scannow
-if exist "C:\Windows.old" rmdir /s /q "C:\Windows.old"
-exit
